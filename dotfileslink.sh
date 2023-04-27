443afc9936d8499ad8c217f5a3b1d4ea21874656
Daiki Wakabayashi <apollo.daiki@gmail.com>
2023-04-27 11:36:52 +0900

    update init.vim

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
+
+##########
+# set up #
+##########
+
+# setup neovim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# link
 ln -sf ~/dotfiles/nvim ~/.config/nvim
