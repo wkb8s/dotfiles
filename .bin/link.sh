46dbceb81d2c498e14214a07796b0d5e8fd6fe6c
Daiki Wakabayashi <apollo.daiki@gmail.com>
2023-09-10 11:40:48 +0900

    update dotfiles

--- .bin/link.sh
+++ .bin/link.sh
@@ -4,8 +4,9 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 for dotfile in "${SCRIPT_DIR}"/.??* ; do
     [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
-    # [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
 
     ln -fnsv "$dotfile" "$HOME"
 done
+
+ln -fnsv "Brewfile" "$HOME"
