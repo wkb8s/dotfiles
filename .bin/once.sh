ef621dc967c6528db0ea92cd216e9d42828c2ed2
Daiki Wakabayashi <apollo.daiki@gmail.com>
2023-09-10 11:01:45 +0900

    update dotfiles

--- .bin/once.sh
+++ .bin/once.sh
@@ -12,4 +12,4 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # enhancd
-git clone https://github.com/b4b4r07/enhancd && source enhancd/init.sh
+git clone https://github.com/b4b4r07/enhancd ~/.zsh
