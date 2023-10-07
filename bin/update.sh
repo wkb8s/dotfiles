ca0ebcc46b6e478f04fd3c6bdd01289d1cf6ee8f
Daiki Wakabayashi <apollo.daiki@gmail.com>
2023-10-07 14:23:36 +0900

    update dotfiles

--- bin/update.sh
+++ bin/update.sh
@@ -5,6 +5,7 @@ if [ "$(uname)" != "Darwin" ] ; then
 	exit 1
 fi
 
+# brew
 brew update
 brew upgrade
 # brew upgrade --cask --greedy
@@ -12,9 +13,14 @@ brew bundle --global
 brew bundle cleanup --global --force
 brew cleanup
 
+# python
 pip3 install --upgrade pip
 if type "pip-review" > /dev/null 2>&1; then
   pip-review --auto
 else
   echo "pip-review not exist!"
 fi
+
+# cleanup cashes
+sudo rm -rf /System/Library/Caches/* /Library/Caches/* ~/Library/Caches/*
+sudo rm -r /private/var/vm/sleepimage
